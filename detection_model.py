1324a8b493e8a062b0117d3805a1dbb84064775e
yeongjoon <go1217jo@naver.com>
2019-06-25 19:45:48 +0900

    add to predict the number of people in each table

--- detection_model.py
+++ detection_model.py
@@ -12,7 +12,7 @@ class FaceDetector(object):
     def __init__(self, score=0.3, iou=0.45, gpu_num=1):
         self.sess = K.get_session()
         self.anchors = np.array([10.0,13.0, 16.0,30.0, 33.0,23.0, 30.0,61.0, 62.0,45.0, 59.0,119.0, 116.0,90.0, 156.0,198.0, 373.0,326.0]).reshape(-1, 2)
-        self.model_image_size = (480,480)
+        self.model_image_size = (416,416)
         self.score = score
         self.iou = iou
         self.input_image_shape = K.placeholder(shape=(2,))
@@ -23,7 +23,22 @@ class FaceDetector(object):
         assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
 
         num_anchors = len(anchors)
-        num_classes = 1
+        self.coco_classes = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus",
+                   "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
+                   "parking meter", "bench", "bird", "cat", "dog", "horse",
+                   "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+                   "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+                   "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+                   "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                   "fork", "knife", "spoon", "bowl", "banana", "apple",
+                   "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
+                   "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                   "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard",
+                   "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+                   "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+        #num_classes = 1
+        num_classes = len(self.coco_classes)
 
         model = None
         try:
@@ -43,11 +58,12 @@ class FaceDetector(object):
 
 
     def load_model(self, yolo_model_path):
-        self.yolo_model = self.generate(yolo_model_path, self.anchors, 0.2)
+        self.yolo_model = self.generate(yolo_model_path, self.anchors, 0.3)
 
 
     def detect(self, frame):
-        faces = []
+        objs = []
+        class_names = []
         if self.model_image_size != (None, None):
             assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
             assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
@@ -68,19 +84,22 @@ class FaceDetector(object):
                 #K.learning_phase(): 0
             })
 
-        # face detection
+        # detection
         confidences = []
         for i, score in enumerate(scores):
             box = boxes[i]
-            y1, x1, y2, x2 = box
-            y1 = max(0, np.floor(y1 + 2.5).astype('int32'))
-            x1 = max(0, np.floor(x1 + 2.5).astype('int32'))
-            y2 = min(frame.size[1], np.floor(y2 + 2.5).astype('int32'))
-            x2 = min(frame.size[0], np.floor(x2 + 2.5).astype('int32'))
-            confidences.append(score)
-            faces.append((x1, y1, x2, y2))
+            class_name = self.coco_classes[classes[i]]
+            if class_name == 'person':
+                y1, x1, y2, x2 = box
+                y1 = max(0, np.floor(y1 + 2.5).astype('int32'))
+                x1 = max(0, np.floor(x1 + 2.5).astype('int32'))
+                y2 = min(frame.size[1], np.floor(y2 + 2.5).astype('int32'))
+                x2 = min(frame.size[0], np.floor(x2 + 2.5).astype('int32'))
+                confidences.append(score)
+                objs.append((x1, y1, x2, y2))
+                class_names.append(class_name)
         
-        return faces, confidences
+        return objs, class_names, confidences
 
 
     def close_session(self):
--- run_keras_server.py
+++ run_keras_server.py
@@ -13,8 +13,9 @@ class DetectModel(object):
     def __init__(self):
         self.num_of_people = 0
         self.face_model = FaceDetector()
-        self.face_model.load_model('model_data/yolo_face_model.h5')
-        self.vidcap = cv2.VideoCapture(0)
+        self.face_model.load_model('model_data/yolo_weights.h5')
+        self.vidcap = cv2.VideoCapture('data/20190625_134522.mp4')
+        self.table = [0, 0, 0, 0]
 
         if not self.vidcap.isOpened():
             raise IOError("Couldn't open camera")
@@ -23,25 +24,75 @@ class DetectModel(object):
         self.thread.start()
 
     def detect(self):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
         while True:
             cnt = []
+            tables = []
             for i in range(3):
+                all_num = 0
+                t_num = [0, 0, 0, 0]
                 return_value, frame = self.vidcap.read()
                 if not return_value:
                     break
+
+                frame = frame[330:-200, :]
+                height, width, _ = frame.shape
+
                 image = Image.fromarray(frame)
-                boxes, confidence = self.face_model.detect(image)
-                print(boxes)
-                cnt.append(len(boxes))
+                boxes, classes, confidence = self.face_model.detect(image)
+
+                for k, box in enumerate(boxes):
+                    (x1, y1, x2, y2) = box
+                    if y1 > 10:
+                        all_num += 1
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                        cv2.putText(frame, classes[k], (x1, y1), font, 1.5, (0, 255, 255))
+
+                        pos_x = (x1 + x2) / 2
+                        pos_y = (y1 + y2) / 2
+                        
+                        # left part
+                        if pos_x < width / 2:
+                            # table 2
+                            if pos_y > height / 2 - 100:
+                                t_num[1] += 1
+                            # table 1
+                            else:
+                                t_num[0] += 1
+                        # right part
+                        else:
+                            # table 4
+                            if pos_y > height / 2 - 100:
+                                t_num[3] += 1
+                            # table 3
+                            else:
+                                t_num[2] += 1
+                    
+                cv2.imwrite('img'+str(i)+'.png', frame)
+                
+                cnt.append(all_num)
+                tables.append(t_num)
+                time.sleep(2)
             
             self.num_of_people = max(cnt)
+            self.num_of_tables = [0, 0, 0, 0]
+            for t in range(4):
+                self.num_of_tables[t] = max([tables[0][t], tables[1][t], tables[2][t]])
 
-            time.sleep(60)
+            time.sleep(15)
 
 @app.route("/numofpeople", methods=["GET"])
 def get_num_of_people():
     data = {"num": model.num_of_people}
     return flask.jsonify(data)
+
+@app.route("/tablestatus", methods=["GET"])
+def get_table_status():
+    data = {"t1": model.num_of_tables[0], "t2": model.num_of_tables[1],
+            "t3": model.num_of_tables[2], "t4": model.num_of_tables[3]}
+    return flask.jsonify(data)
+    
 
 if __name__ == "__main__":
     print("Loading keras model")
